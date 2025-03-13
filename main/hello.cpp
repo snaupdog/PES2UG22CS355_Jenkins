6d1e37d63ec887eec27cebb20d89565d03d543f8
Nihal Satish <nihal.satish@gmail.com>
2025-03-13 12:28:01 +0530

    first changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,10 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this rn  \n";
   return 0;
 }
