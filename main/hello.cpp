7a38e645a9bb08a35bf2451b1f01f85ee893d9e6
Nihal Satish <nihal.satish@gmail.com>
2025-03-13 14:53:49 +0530

    second change

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this rn  \n";
+  cout << "I have successfully built and run this rn after i made a second "
+          "change  \n";
   return 0;
 }
